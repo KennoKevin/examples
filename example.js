345f8368d1b0a76218a11e7e546d010190dcb625
Kenno Kevin <145542877+KennoKevin@users.noreply.github.com>
2023-11-22 09:40:47 +0200

    Add files via upload

---
+++ example.js
@@ -0,0 +1,13 @@
+//loop on kasulik kui tahad enda koodi käivitada mitmeid kordi aga teise väärtusega.
+for (let i = 0; i < headphones.length; i++) {
+    item += headphones[i] + "<br>";
+  }
+
+  //muutujad
+  //muutuja luuakse ehk deklareeritakse käsusõnaga var.
+  var colour = "black" //näitab et muutuja "colour" saab väärtuseks "black"
+  //muutuja väärtuse sisestamine programmi kasutaja poolt
+  var age = prompt("how old are you?");
+
+  //andmetüübid (5)
+  
